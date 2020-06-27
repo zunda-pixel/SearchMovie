433ed3f8b26cc73f4851b6b89945169f01225dc6
zunda-pixel <zunda.pixel@gmail.com>
2020-06-27 19:25:11 +0900

    Initial Commit

---
+++ SearchMovie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchMovie
+//
+//  Created by zunda on 2020/06/27.
+//  Copyright © 2020 zunda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
